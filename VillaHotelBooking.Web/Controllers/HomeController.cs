3dbbd1d05b69498a1eca79677edeff4ecd28f617
Hakam Chedo <ha-kam009@hotmail.com>
2024-10-02 19:44:23 +0700

    Availabelity and Reload Only partial view

--- VillaHotelBooking.Web/Controllers/HomeController.cs
+++ VillaHotelBooking.Web/Controllers/HomeController.cs
@@ -25,6 +25,34 @@ namespace VillaHotelBooking.Web.Controllers
             return View(homeVM);
         }
 
+        // POST: HomeController/Index
+        [HttpPost]
+        public IActionResult Index(HomeVM homeVM)
+        {
+            homeVM.VillaList = _unitOfWork.Villas.GetAll(includeProperties: "VillaAmenity");
+            
+            return View(homeVM);
+        }
+
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villas.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsVailable = false;
+                }
+            }
+            HomeVM homeVM = new()
+            {
+                VillaList = villaList,
+                Nights = nights,
+                CheckInDate = checkInDate,
+            };
+            return PartialView("_VillaList", homeVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
