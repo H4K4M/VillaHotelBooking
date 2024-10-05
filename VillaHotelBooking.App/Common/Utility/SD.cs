1a3c6b6429f112a08a5bfaa64de5c4e0ed4ba904
Hakam Chedo <ha-kam009@hotmail.com>
2024-10-05 07:18:43 +0700

    VillaRoomAvailable_Count in SD utility

--- VillaHotelBooking.App/Common/Utility/SD.cs
+++ VillaHotelBooking.App/Common/Utility/SD.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VillaHotelBooking.Domain.Entities;
 
 namespace VillaHotelBooking.App.Common.Utility
 {
@@ -17,5 +18,38 @@ namespace VillaHotelBooking.App.Common.Utility
         public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static int VillaRoomAvailable_Count(int villaId, List<VillaNumber> villaNumberList,
+            DateOnly checkInDate, int nights, List<Booking> bookings)
+        {
+            List<int> bookingInDate = new();
+            int finalAvailableRoomsForAllNights = int.MaxValue;
+            var roomsInVilla = villaNumberList.Where(u => u.VillaId == villaId).Count();
+
+            for(int i = 0; i < nights; i++)
+            {
+                var vallasbooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i) 
+                && u.CheckOutDate > checkInDate.AddDays(i) && u.VillaId == villaId);
+
+                foreach(var booking in vallasbooked)
+                {
+                    if (!bookingInDate.Contains(booking.Id))
+                    {
+                        bookingInDate.Add(booking.Id);
+                    }
+                }
+
+                var totalAvailableRooms = roomsInVilla - bookingInDate.Count;
+                if(totalAvailableRooms == 0)
+                {
+                    return 0;
+                }
+                else if (totalAvailableRooms < finalAvailableRoomsForAllNights)
+                {
+                    finalAvailableRoomsForAllNights = totalAvailableRooms;
+                }
+            }
+            return finalAvailableRoomsForAllNights;
+        }
     }
 }
