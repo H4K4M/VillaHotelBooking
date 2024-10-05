738ec082160cc190c8bd4be2585b55d3e5639546
Hakam Chedo <ha-kam009@hotmail.com>
2024-10-05 07:55:04 +0700

    Room Availablity

--- VillaHotelBooking.Web/Controllers/HomeController.cs
+++ VillaHotelBooking.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using VillaHotelBooking.App.Common.Interfaces;
+using VillaHotelBooking.App.Common.Utility;
 using VillaHotelBooking.Web.Models;
 using VillaHotelBooking.Web.ViewModels;
 
@@ -30,12 +31,15 @@ namespace VillaHotelBooking.Web.Controllers
         public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
         {
             var villaList = _unitOfWork.Villas.GetAll(includeProperties: "VillaAmenity").ToList();
+            var villaNumberList = _unitOfWork.VillaNumbers.GetAll().ToList();
+            var bookedVillas = _unitOfWork.Bookings.GetAll(u => u.Status == SD.StatusApproved || u.Status == SD.StatusCheckedIn).ToList();
+
+
             foreach (var villa in villaList)
             {
-                if (villa.Id % 2 == 0)
-                {
-                    villa.IsVailable = false;
-                }
+                int roomAvailable = SD.VillaRoomAvailable_Count(villa.Id, villaNumberList, checkInDate, nights, bookedVillas);
+
+                villa.IsVailable = roomAvailable > 0 ? true : false;
             }
             HomeVM homeVM = new()
             {
