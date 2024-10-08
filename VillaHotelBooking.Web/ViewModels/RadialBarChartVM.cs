121eba03d7bfc2542c417cd98e8ffd8682e619c0
Hakam Chedo <ha-kam009@hotmail.com>
2024-10-08 12:31:58 +0700

    radialChart

---
+++ VillaHotelBooking.Web/ViewModels/RadialBarChartVM.cs
@@ -0,0 +1,10 @@
+﻿namespace VillaHotelBooking.Web.ViewModels
+{
+    public class RadialBarChartVM
+    {
+        public decimal TotalCount { get; set; }
+        public decimal CountInCurrentMonth { get; set; }
+        public bool hasRatioIncrease { get; set; }
+        public int[] Series { get; set; }
+    }
+}
---
+++ VillaHotelBooking.Web/wwwroot/js/dashboard/getTotalUserRadialChart.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    loadTotalUserRadialChart();
+});
+
+function loadTotalUserRadialChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetRegisteredUserRadialChartData",
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+            document.querySelector("#spanTotalUserCount").innerHTML = data.totalCount;
+
+            var sectionCurrentCount = document.createElement("span");
+            if (data.hasRatioIncrease) {
+                sectionCurrentCount.className = "text-success me-1";
+                sectionCurrentCount.innerHTML = `<i class="bi bi-arrow-up-right-circle me-1"></i><span> ${data.countInCurrentMonth} </span>`;
+            }
+            else {
+                sectionCurrentCount.className = "text-danger me-1";
+                sectionCurrentCount.innerHTML = `<i class="bi bi-arrow-down-right-circle me-1"></i><span> ${data.countInCurrentMonth} </span>`;
+            }
+
+            document.querySelector("#sectionUserCount").append(sectionCurrentCount);
+            document.querySelector("#sectionUserCount").append("Since last month");
+
+            loadRadialBarChart("totalUserRadialChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
+
